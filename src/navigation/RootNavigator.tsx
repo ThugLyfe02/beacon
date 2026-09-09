420d5b73e063dc008d813b4628ae7b24c735ebd4
Benjamin Birnbaum <132301476+ThugLyfe02@users.noreply.github.com>
2026-09-09 00:12:21 -0400

    feat(constellation): register Machine Lab in operator navigation

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -36,6 +36,7 @@ import InternalCasebookScreen from '../screens/InternalCasebookScreen';
 import InternalPatternLabScreen from '../screens/InternalPatternLabScreen';
 import InternalForensicsLabScreen from '../screens/InternalForensicsLabScreen';
 import InternalMissionLedgerScreen from '../screens/InternalMissionLedgerScreen';
+import InternalMachineLabScreen from '../screens/InternalMachineLabScreen';
 import InternalOperatorHubScreen from '../screens/InternalOperatorHubScreen';
 import SpatialFieldScreen from '../spatial/SpatialFieldScreen';
 import ChooseAvatarScreen from '../screens/ChooseAvatarScreen';
@@ -198,6 +199,7 @@ export function RootNavigator() {
         <Stack.Screen name="InternalGraph" component={InternalGraphScreen} options={internalModalOptions} />
         <Stack.Screen name="InternalForensicsLab" component={InternalForensicsLabScreen} options={internalModalOptions} />
         <Stack.Screen name="InternalMissionLedger" component={InternalMissionLedgerScreen} options={internalModalOptions} />
+        <Stack.Screen name="InternalMachineLab" component={InternalMachineLabScreen} options={internalModalOptions} />
         <Stack.Screen name="InternalBridgeLab" component={InternalBridgeLabScreen} options={internalModalOptions} />
         <Stack.Screen name="InternalStrategyLab" component={InternalStrategyLabScreen} options={internalModalOptions} />
         <Stack.Screen name="InternalSimulationLab" component={InternalSimulationLabScreen} options={internalModalOptions} />
